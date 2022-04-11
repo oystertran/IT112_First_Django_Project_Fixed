f4a3f1b41d1b6ce95987f6ad9c2ac5ee66076103
HAO TRAN <quanghao81111@gmail.com>
2022-04-11 13:43:39 -0700

    Assignment 4 Model commit

--- DjangoProject/PythonClub/admin.py
+++ DjangoProject/PythonClub/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PythonClubType, Product, Review
 
 # Register your models here.
+
+admin.site.register(PythonClubType)
+admin.site.register(Product)
+admin.site.register(Review)
