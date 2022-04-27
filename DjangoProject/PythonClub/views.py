508204cdadc8680657438760da6c5b01f8bc1206
HAO TRAN <quanghao81111@gmail.com>
2022-04-27 12:06:58 -0700

    second product view commit

--- DjangoProject/PythonClub/views.py
+++ DjangoProject/PythonClub/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Product, PythonClubType, Review
 
 # Create your views here.
 def index(request):
     return render(request,'PythonClub/index.html')
+
+def products(request):
+    product_list = Product.objects.all()
+    return render (request, 'PythonClub/products.html', 
+    {'product_list': product_list})
+
+
